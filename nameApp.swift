7827bbc024c560c34389a95d2996b59d6881c5f2
simonegallinari <simone.gallinari@gmail.com>
2023-03-02 13:58:22 +0100

    adsadasd

---
+++ nameApp.swift
@@ -0,0 +1,17 @@
+//
+//  nameApp.swift
+//  name
+//
+//  Created by Simone  Gallinari  on 27/02/23.
+//
+
+import SwiftUI
+
+@main
+struct nameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
