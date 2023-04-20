d2a8dcd5a807557d82d2252b495462941ccde25f
Galloooh <simone.gallinari@gmail.com>
2023-04-20 15:08:32 +0200

    mappa

--- Giochino/src/map.hpp
+++ Giochino/src/map.hpp
@@ -11,12 +11,20 @@
 #include <stdio.h>
 using namespace std;
 
+#define MAP_DIMENSION 300
+
 class Map{
+protected:
+	char pattern0[MAP_DIMENSION];
+	char pattern1[MAP_DIMENSION];
+	char pattern2[MAP_DIMENSION];
+	char pattern3[MAP_DIMENSION];
+	char pattern4[MAP_DIMENSION];
 public:
     Map (int colonna, int riga);
     void draw(int x, int y, char P);
     void canc(int x, int y, char P);
-
+    void initPattern();
 
 };
 #endif /* map_hpp */
